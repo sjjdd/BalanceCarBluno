f2b6a6ed7d6c5f610d0fcae453c01fbaf6b97fd6
unknown <1667688587@qq.com>
2018-02-24 14:47:12 +0800

    first commit

---
+++ app/src/main/java/company/sunjunjie/come/balanacecarbluno/MainActivity.java
@@ -0,0 +1,145 @@
+package company.sunjunjie.come.balanacecarbluno;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends BlunoLibrary {
+
+    private Button buttonScan;
+    private Button buttonSerialSend,buttonForward,buttonLeft,buttonRight,buttonBottom;
+    private EditText serialSendText;
+    private TextView serialReceivedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onCreateProcess();														//onCreate Process by BlunoLibrary
+
+
+        serialBegin(115200);													//set the Uart Baudrate on BLE chip to 115200
+
+        serialReceivedText=(TextView) findViewById(R.id.serialReveicedText);	//initial the EditText of the received data
+        serialSendText=(EditText) findViewById(R.id.serialSendText);			//initial the EditText of the sending data
+
+        buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);		//initial the button for sending the data
+        buttonSerialSend.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                serialSend(serialSendText.getText().toString());				//send the data to the BLUNO
+            }
+        });
+        //控制前后左右的按钮，这里象征性的发送一些字符
+       buttonForward=(Button) findViewById(R.id.forward);
+        buttonForward.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serialSend("A");
+            }
+        });
+        buttonLeft=(Button) findViewById(R.id.left);
+        buttonLeft.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serialSend("B");
+            }
+        });
+        buttonRight=(Button) findViewById(R.id.right);
+        buttonRight.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serialSend("C");
+            }
+        });
+       buttonBottom=(Button) findViewById(R.id.backward);
+        buttonBottom.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serialSend("D");
+            }
+        });
+        buttonScan = (Button) findViewById(R.id.buttonScan);					//initial the button for scanning the BLE device
+        buttonScan.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device
+            }
+        });
+    }
+
+    protected void onResume(){
+        super.onResume();
+        System.out.println("BlUNOActivity onResume");
+        onResumeProcess();														//onResume Process by BlunoLibrary
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        onPauseProcess();														//onPause Process by BlunoLibrary
+    }
+
+    protected void onStop() {
+        super.onStop();
+        onStopProcess();														//onStop Process by BlunoLibrary
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        onDestroyProcess();														//onDestroy Process by BlunoLibrary
+    }
+
+    @Override
+    public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
+        switch (theConnectionState) {											//Four connection state
+            case isConnected:
+                buttonScan.setText("Connected");
+                break;
+            case isConnecting:
+                buttonScan.setText("Connecting");
+                break;
+            case isToScan:
+                buttonScan.setText("Scan");
+                break;
+            case isScanning:
+                buttonScan.setText("Scanning");
+                break;
+            case isDisconnecting:
+                buttonScan.setText("isDisconnecting");
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onSerialReceived(String theString) {							//Once connection data received, this function will be called
+        // TODO Auto-generated method stub
+        serialReceivedText.append(theString);							//append the text into the EditText
+        //The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
+        ((ScrollView)serialReceivedText.getParent()).fullScroll(View.FOCUS_DOWN);
+    }
+}
